3463ae7672c21e84c7514d99eaf9403e82c83c06
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2020-11-09 23:25:47 -0700

    Final

--- JS/javascript.js
+++ JS/javascript.js
@@ -7,5 +7,5 @@ function showDate() {
     var curr_month = d.getMonth() + 1;
     var curr_date = d.getDate();
     var curr_year = d.getFullYear();
-    document.write(curr_month + "-" + curr_date + "-" + curr_year);
+    document.write("Today is " + curr_month + "-" + curr_date + "-" + curr_year);
 }
